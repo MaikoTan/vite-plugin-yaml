452f24dcea961380b4fc39ded89c3aafdcf9f787
Maiko Tan <maiko.tan.coding@gmail.com>
2022-12-09 17:17:00 +0800

    refactor: remove all pnpm stuffs

--- example/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import ViteYAML from '@modyfi/vite-plugin-yaml';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), ViteYAML()],
-});
